1ba9d22205f79471d5b6020ca797f76e8f5008a8
suri.kim <suri.kim@daumkakao.com>
2018-09-23 19:15:18 +0900

    Monte Carlo Prediction

---
+++ gridWorld/monteCarlo/agent_mc.py
@@ -0,0 +1,188 @@
+##########################################################################################################
+# Monte Carlo Estimation
+# - DP 에서는 기본적으로 environment dynamics 을 안다고 가정하였으나, 이제 이것을 가정하지 않는다.
+#   sample 에 의한 experience 만 얻을 수 있는 정보이다.
+# - 무작위(random) sample 을 기반으로 complete return (not partial return) 에 대한 평균(mean)을 구한다.
+#       L MC는 bootstrap 이 아니다.
+#               L bootstrap : 다른 state의 state-value 예상값에 의존하여 현재 state 의 state-value 값을 예측하는 방법
+# - 더 많은 return 을 얻을수록 그 평균치는 기대치에 수렴할 것이다.
+#
+# (1) 경험을 통한 학습 :
+#       MC method 는 environment 와의 모의(simulated) 또는 실제(actual) interaction 을 통해 얻은
+#       states, action, rewards 가 포함된 sample 이라고 불리우는 경험(experience)을 이용한다.
+# (2) 에피소드 단위의 학습 :
+#       episodic tasks 에 대해 적용하는 것을 가정한다. 즉, step-by-step 보다는 episode-by-episode 단위의 학습이다.
+#
+# Monte Carlo Method
+# (1) First-visit MC method :
+#       하나의 episode 에서 state s를 처음 visit 후 얻은 return 값의 평균을 사용해서 Vㅠ(s)를 구한다.
+#       (하나의 episode 에서 다시 접한 state s는 state-value function 평균치를 구하는데 사용하지 않는다.)
+# (2) Every-visit MC method :
+#       하나의 episode 에서 state s의 모든 visit 을 고려하여 return 값의 평균을 계산한다.
+##########################################################################################################
+
+# Model 을 알수 없다면 q*를 구하는 것이 유용하다.
+# 염려되는 점이라면, 한번도 접하지 않은 state-action pair가 있을 수 있다는 사실이다. 끊임없는 exploration 을 보장해야 한다.
+# 모든 state-action pair 를 접해볼 수 있도록 보장하는 가장 흔한 대안은 non-zero probability 를 가진 stochastic policy 만을 고려하는 것이다.
+# ######################################################################################
+import numpy as np
+import random
+from collections import defaultdict
+from gridWorld.monteCarlo.environment import Env
+
+
+class MonteCarlo:
+    def __init__(self, actions):
+        self.width = 5
+        self.height = 5
+        self.actions = actions      # 상,하,좌,우 -> [0, 1, 2, 3]
+        self.learning_rate = 0.01   # step size
+        self.discount_factor = 0.9  # 감가율
+        self.epsilon = 0.1          # 입실론
+        self.samples = []           # 하나의 episode 에 대한 모든 trajectory 의 next_state, reward, done 저장
+        self.value_table = defaultdict(float)   # 에이전트가 방문한 state 의 value function 값 저장
+
+    # 메모리에 sample 저장
+    def save_sample(self, state, reward, done):
+        self.samples.append([state, reward, done])
+
+    """
+    에피소드에서 에이전트가 방문한 state 의 value function 업데이트
+    
+    <first visit method> 
+            samples: [[[0, 0], 0, False],
+                      [[0, 0], 0, False],
+                      [[0, 0], 0, False],
+                      [[1, 0], 0, False],
+                      [[0, 0], 0, False],
+                      [[1, 0], 0, False],
+                      [[2, 0], 0, False],
+                      [[2, 0], 0, False],
+                      [[2, 0], 0, False],
+                      [[2, 1], -100, True]]
+            계산되는 sample: [[2, 1], -100, True]
+                           [[2, 0], 0, False]
+                           [[1, 0], 0, False]
+                           [[0, 0], 0, False]
+    """
+    def update(self):
+        return_s = 0
+        visit_state = []
+
+        # 하나의 에피소드의 모든 샘플에 대해
+        print("samples : ", self.samples)
+        for sample in reversed(self.samples):
+            state_s = str(sample[0])    # 예) '[1,2]'
+
+            # 첫번째 visit 에 대한 것만 평균값 계산 -> 두번 이상 방문한 state with Return G는 계산되지 않는다.
+            if state_s not in visit_state:
+                print("-> 계산되는 sample : ", sample)
+                visit_state.append(state_s)
+                # reverse 해서 사용하므로 G6 -> G5 -> G4 -> G3 -> G2 -> G1의 형태로 계산된다. (책 44P)
+                return_s = self.discount_factor * (sample[1] + return_s)
+                # 현재 state 의 value function 값
+                value_s = self.value_table[state_s]
+                # return 평균값으로 state 의 value function 값 업데이트
+                self.value_table[state_s] = (value_s + self.learning_rate * (return_s - value_s))  # (PT 2P)
+
+    """
+    입실론 탐욕 정책에 따라서 action 반환
+        :param      state  -> [x,y]
+        :return     action -> 0 ~ 3 중 하나
+    """
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            # 입실론 보다 적으면 랜덤 action([0, 1, 2, 3]) 중 하나 반환
+            action = np.random.choice(self.actions)
+        else:
+            # state 에서 취할 수 있는 action 의 value function 값 얻기
+            value = self.possible_state_value(state)
+            # value function 이 최대값인 action 반환
+            action = self.arg_max(value)
+        return int(action)
+
+    @staticmethod
+    def arg_max(args):
+        max_index_list = []
+        max_value = args[0]   # 첫 번째 값을 max 설정해서 비교
+
+        # 최대값 계산
+        for index, value in enumerate(args):
+            if value > max_value:
+                max_index_list.clear()
+                max_value = value
+                max_index_list.append(index)
+            elif value == max_value:
+                max_index_list.append(index)
+        # 무작위로 인덱스 하나 반환
+        return random.choice(max_index_list)
+
+    """
+    state 에서 취할 수 있는 action 의 value function 값 반환
+    :param      state  -> [x,y]
+    :return     state_value : 모든 action(상,하,좌,우)의 value function 값을 담은 리스트
+    """
+    def possible_state_value(self, state):
+        col, row = state
+        state_value = [0.0] * 4     # [0.0, 0.0, 0.0, 0.0]
+
+        # [1] 상
+        if row != 0:
+            state_value[0] = self.value_table[str([col, row - 1])]
+        else:
+            state_value[0] = self.value_table[str(state)]   # 그리드 범위를 넘어가면 현재 state 적용
+
+        # [2] 하
+        if row != self.height - 1:
+            state_value[1] = self.value_table[str([col, row + 1])]
+        else:
+            state_value[1] = self.value_table[str(state)]
+
+        # [3] 좌
+        if col != 0:
+            state_value[2] = self.value_table[str([col - 1, row])]
+        else:
+            state_value[2] = self.value_table[str(state)]
+
+        # [4] 우
+        if col != self.width - 1:
+            state_value[3] = self.value_table[str([col + 1, row])]
+        else:
+            state_value[3] = self.value_table[str(state)]
+
+        # print("next_state = ", next_state)
+        # [0.0, 0.0, 0.0, -0.7290000000000001]
+        #  ...
+        return state_value
+
+
+if __name__ == "__main__":
+    env = Env()
+    agent = MonteCarlo(actions=list(range(env.n_actions)))  # 4 : 상(up), 하(down), 좌(left), 우(right)
+
+    # generate an episodes
+    for episode in range(10):   # 1000
+        state = env.reset()
+
+        # 입실론 탐욕 정책에 따라서 특정 state 의 action 얻기
+        action = agent.get_action(state)
+
+        while True:
+            env.render()
+
+            # 현재 state에서 action 취하기
+            # 환경으로 부터 다음 상태(next_state)와 보상(reward)을 받는다.
+            #   L state)는 리스트, 보상(reward)은 숫자, 완료 여부(done)는 boolean
+            next_state, reward, done = env.step(action)
+            # 메모리에 sample 저장 : 몬테카를로 예측에서는 Rt+1, St+1 을 샘플로 사용한다.
+            agent.save_sample(next_state, reward, done)
+            # 다음 상태(state)에서 취할 수 있는 action 얻기
+            action = agent.get_action(next_state)
+
+            # 하나의 에피소드가 완료되어야만 지나온 모든 state 의 value function 을 업데이트한다.
+            # 에피소드가 끝이 없거나 길이가 긴 경우에는 몬테카를로 예측은 적합하지 않다.
+            if done:
+                agent.update()
+                agent.samples.clear()
+                break
+
---
+++ gridWorld/monteCarlo/environment.py
@@ -0,0 +1,117 @@
+import time
+import numpy as np
+import tkinter as tk
+from PIL import ImageTk, Image
+
+np.random.seed(1)
+PhotoImage = ImageTk.PhotoImage
+UNIT = 100      # 픽셀 수
+HEIGHT = 5      # 그리드월드 세로
+WIDTH = 5       # 그리드월드 가로
+
+
+class Env(tk.Tk):
+    def __init__(self):
+        super(Env, self).__init__()
+        self.action_space = ['u', 'd', 'l', 'r']    # 상(up), 하(down), 좌(left), 우(right)
+        self.n_actions = len(self.action_space)
+        self.title('monte carlo')
+        self.geometry('{0}x{1}'.format(HEIGHT * UNIT, HEIGHT * UNIT))
+        self.shapes = self.load_images()
+        self.canvas = self._build_canvas()
+        self.texts = []
+
+    def _build_canvas(self):
+        canvas = tk.Canvas(self, bg='white',
+                           height=HEIGHT * UNIT,
+                           width=WIDTH * UNIT)
+        # 그리드 생성
+        for c in range(0, WIDTH * UNIT, UNIT):  # 0~400 by 80
+            x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
+            canvas.create_line(x0, y0, x1, y1)
+        for r in range(0, HEIGHT * UNIT, UNIT):  # 0~400 by 80
+            x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
+            canvas.create_line(x0, y0, x1, y1)
+
+        # 캔버스에 이미지 추가
+        self.rectangle = canvas.create_image(50, 50, image=self.shapes[0])
+        self.triangle1 = canvas.create_image(250, 150, image=self.shapes[1])
+        self.triangle2 = canvas.create_image(150, 250, image=self.shapes[1])
+        self.circle = canvas.create_image(250, 250, image=self.shapes[2])
+
+        canvas.pack()
+
+        return canvas
+
+    @staticmethod
+    def load_images():
+        rectangle = PhotoImage(Image.open("../img/rectangle.png").resize((65, 65)))
+        triangle = PhotoImage(Image.open("../img/triangle.png").resize((65, 65)))
+        circle = PhotoImage(Image.open("../img/circle.png").resize((65, 65)))
+
+        return rectangle, triangle, circle
+
+    """
+    (정적 메소드 선언)
+    ---------------
+        :return 좌표 정보를 state 로 반환
+    """
+    @staticmethod
+    def coords_to_state(coords):
+        x = int((coords[0] - 50) / 100)
+        y = int((coords[1] - 50) / 100)
+        return [x, y]
+
+    def reset(self):
+        self.update()
+        time.sleep(0.5)
+        x, y = self.canvas.coords(self.rectangle)
+        self.canvas.move(self.rectangle, UNIT / 2 - x, UNIT / 2 - y)
+        return self.coords_to_state(self.canvas.coords(self.rectangle))
+
+    def step(self, action):
+        state = self.canvas.coords(self.rectangle)
+        base_action = np.array([0, 0])
+        self.render()
+
+        # action 에 따라 current state 에서 next state 까지 이동해야 할 크기 계산
+        if action == 0:    # 상
+            if state[1] > UNIT:
+                base_action[1] -= UNIT
+        elif action == 1:  # 하
+            if state[1] < (HEIGHT - 1) * UNIT:
+                base_action[1] += UNIT
+        elif action == 2:  # 좌
+            if state[0] > UNIT:
+                base_action[0] -= UNIT
+        elif action == 3:  # 우
+            if state[0] < (WIDTH - 1) * UNIT:
+                base_action[0] += UNIT
+
+        # 에이전트(빨간 네모) 이동 : 현재 state 에서 base_action 만큼 이동
+        self.canvas.move(self.rectangle, base_action[0], base_action[1])
+        # 에이전트(빨간 네모)를 맨 앞으로 배치 (다른 것에의해 가리지 않도록)
+        self.canvas.tag_raise(self.rectangle)
+        # 이동한 좌표 정보 얻기
+        next_state = self.canvas.coords(self.rectangle)
+
+        # 보상(reward) 계산
+        if next_state == self.canvas.coords(self.circle):
+            reward = 100
+            done = True
+        elif next_state in [self.canvas.coords(self.triangle1), self.canvas.coords(self.triangle2)]:
+            reward = -100
+            done = True
+        else:
+            reward = 0
+            done = False
+
+        # 이동한 좌표 정보를 state 로 얻기
+        next_state = self.coords_to_state(next_state)
+
+        # 상태(state)는 리스트, 보상(reward)은 숫자, 완료 여부(done)는 boolean
+        return next_state, reward, done
+
+    def render(self):
+        time.sleep(0.03)
+        self.update()
